48409dc63793b72b821c2d66b238c65887f88f71
Omochice <mochice.mls.ntl@gmail.com>
2025-02-27 11:14:50 +0900

    feat: add sheepla/duckgo

---
+++ pkgs/duckgo/default.nix
@@ -0,0 +1,27 @@
+{
+  source,
+  lib,
+  buildGoModule,
+}:
+
+buildGoModule rec {
+  inherit (source) pname src version;
+
+  vendorHash = "sha256-njoBUoc4nB8q54lEPHvugM8CCHYwB0sklg0x8lyKK7Y=";
+
+  ldflags = [
+    "-X main.appRevison=unknown"
+    "-X main.appVersion=${version}"
+  ];
+
+  CGO_ENABLED = 0;
+
+  doCheck = false;
+
+  meta = with lib; {
+    description = "A command line DuckDuckGo client with fuzzyfinder UI written in Go";
+    homepage = "https://github.com/sheepla/duckgo";
+    changelog = "https://github.com/sheepla/duckgo/releases/tag/v${version}";
+    license = licenses.mit;
+  };
+}
