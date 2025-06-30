52432bec4ae0dfc87347e03a644363ee0012373d
Omochice <mochice.mls.ntl@gmail.com>
2025-06-30 22:08:10 +0900

    fix: use npm package name as `packageName`
    It seems be used in symlink phase and it must be npm package name.

--- pkgs/claude-code/default.nix
+++ pkgs/claude-code/default.nix
@@ -8,9 +8,9 @@
 nodeEnv.buildNodePackage rec {
   inherit (source) pname src version;
   name = source.pname;
-  packageName = source.pname;
   buildInputs = globalBuildInputs;
 
+  packageName = "@anthropic-ai/claude-code";
   meta = with lib; {
     description = "Agentic coding tool that lives in your terminal, understands your codebase, and helps you code faster";
     homepage = "https://github.com/anthropics/claude-code";
