6dfa42acc80671fc45aae03b4f779f635f5fd328
Omochice <44566328+Omochice@users.noreply.github.com>
2025-10-24 22:25:21 +0900

    feat: add k1LoW/gh-grep (#311)
    * feat: add k1LoW/gh-grep
    
    * fixup! feat: add k1LoW/gh-grep
    
    * fixup
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ pkgs/gh-grep/default.nix
@@ -0,0 +1,29 @@
+{
+  source,
+  lib,
+  buildGoModule,
+}:
+
+buildGoModule rec {
+  inherit (source) pname src version;
+  # keep-sorted start block=yes
+  doCheck = false;
+  env.CGO_ENABLED = 0;
+  ldflags = [
+    "-s"
+    "-w"
+    "-X github.com/k1LoW/gh-grep.version=${version}"
+    "-X github.com/k1LoW/gh-grep.commit=unknown"
+    "-X github.com/k1LoW/gh-grep.date=unknown"
+    "-X github.com/k1LoW/gh-grep/version.Version=${version}"
+  ];
+  meta = with lib; {
+    description = "Print lines matching a pattern in repositories using GitHub API";
+    homepage = "https://github.com/k1LoW/gh-grep";
+    changelog = "https://github.com/k1LoW/gh-grep/releases/tag/${version}";
+    license = licenses.mit;
+    mainProgram = "gh-grep";
+  };
+  vendorHash = "sha256-XEG5fPyDG0tq8WnBhveaIpm0sYAQddf503lyASH6+CI=";
+  # keep-sorted end
+}
