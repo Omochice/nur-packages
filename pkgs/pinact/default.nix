93e5bb0126bcdd8e762d524caa823a8b4a8b4f68
Omochice <mochice.mls.ntl@gmail.com>
2026-02-23 21:28:59 +0900

    chore: flatten

--- pkgs/pinact/default.nix
+++ pkgs/pinact/default.nix
@@ -14,16 +14,14 @@ buildGo126Module rec {
     "-X main.commit=unknown"
     "-X main.version=${version}"
   ];
-  meta = with lib; {
-    description = ''
-      pinact is a CLI to edit GitHub Workflow and Composite action files and pin versions of Actions and Reusable Workflows.
-      pinact can also update their versions and verify version annotations.
-    '';
-    homepage = "https://github.com/suzuki-shunsuke/pinact";
-    changelog = "https://github.com/suzuki-shunsuke/pinact/releases/tag/v${version}";
-    license = licenses.mit;
-    mainProgram = "pinact";
-  };
+  meta.description = ''
+    pinact is a CLI to edit GitHub Workflow and Composite action files and pin versions of Actions and Reusable Workflows.
+    pinact can also update their versions and verify version annotations.
+  '';
+  meta.homepage = "https://github.com/suzuki-shunsuke/pinact";
+  meta.changelog = "https://github.com/suzuki-shunsuke/pinact/releases/tag/v${version}";
+  meta.license = lib.licenses.mit;
+  meta.mainProgram = "pinact";
   subPackages = [ "./cmd/pinact" ];
   vendorHash = "sha256-EqfhHy9OUiaoCI/VFjUJlm917un3Lf4/cUmeHG7w9Bg=";
   # keep-sorted end
